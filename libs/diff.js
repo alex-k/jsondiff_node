80cceddee8325932a86af084673ceacb2daa1db2
alexk <alexk@imac-alexk.local>
2016-02-07 18:33:35 +0100

    flat objects

--- libs/diff.js
+++ libs/diff.js
@@ -2,8 +2,16 @@ module.exports = {
     diff: diff
 }
 
+
 function diff(first, second) {
     var ret = {}
+
+    for (k in second) {
+        if (!first.hasOwnProperty(k)) {
+            ret[k] = second[k];
+            continue;
+        }
+    }
 
     return ret;
 }
--- tests/diff-test.js
+++ tests/diff-test.js
@@ -1,13 +1,21 @@
 var assert = require('assert');
 var module = require('../libs/diff');
 
-describe('jsondiff', function() {
-    describe('#diff()', function() {
-        it('should handle diff on one-dimension arrays', function() {
-            var first = {a:1, b:2};
-            var second = {b:2, c:3};
+describe('jsondiff', function () {
+    describe('#diff()', function () {
+        describe('flat objects', function () {
+            it('should handle unexisted properties', function () {
+                var first = {a: 1, b: 2};
+                var second = {b: 2, c: 3};
 
-            assert.equal({"c":3}, module.diff(first,second));
+                assert.equal(JSON.stringify({"c": 3}), JSON.stringify(module.diff(first, second)));
+            });
+            it('should handle changed properties', function () {
+                var first = {a: 1, b: 2};
+                var second = {a: 1, b: 3};
+
+                assert.equal(JSON.stringify({"b": 3}), JSON.stringify(module.diff(first, second)));
+            });
         });
     });
 });
